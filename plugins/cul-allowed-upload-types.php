b9a5c4fcc0c22ac5a4b2363f3d6f1d3133c156f6
Eric O <eric@ejdigitalmedia.com>
2018-01-23 15:52:12 -0500

    Add initial version of cul-allowed-upload-types plugin

--- plugins/cul-allowed-upload-types.php
+++ plugins/cul-allowed-upload-types.php
@@ -13,19 +13,23 @@ License URI: https://opensource.org/licenses/MIT
 
 define('CUL_ALLOWED_EXTENSIONS_KEY', 'cul_allowed_extensions');
 
-function cul_allowed_upload_types($mimes) {
+function cul_allowed_upload_types($mimes=array()) {
 
   if(get_option(CUL_ALLOWED_EXTENSIONS_KEY) == false || get_option(CUL_ALLOWED_EXTENSIONS_KEY) == '{}') {
     $mimes = array(
     	// Image formats
-    	'jpg|jpeg'                     => 'image/jpeg',
+    	'jpg'                          => 'image/jpeg',
+      'jpeg'                         => 'image/jpeg',
     	'gif'                          => 'image/gif',
     	'png'                          => 'image/png',
     	'bmp'                          => 'image/bmp',
-    	'tif|tiff'                     => 'image/tiff',
+    	'tif'                          => 'image/tiff',
+      'tiff'                         => 'image/tiff',
 
     	// Video formats
-    	'mp4|m4v'                      => 'video/mp4',
+    	'mp4'                          => 'video/mp4',
+      'm4v'                          => 'video/mp4',
+      'avi'                          => 'video/avi',
 
     	// Text formats
     	'txt'                          => 'text/plain',
@@ -34,8 +38,10 @@ function cul_allowed_upload_types($mimes) {
     	// Audio formats
     	'mp3|m4a|m4b'                  => 'audio/mpeg',
     	'wav'                          => 'audio/wav',
-    	'ogg|oga'                      => 'audio/ogg',
-    	'mid|midi'                     => 'audio/midi',
+    	'ogg'                          => 'audio/ogg',
+      'oga'                          => 'audio/ogg',
+  	  'mid'                          => 'audio/midi',
+      'midi'                         => 'audio/midi',
 
     	// Misc application formats
     	'rtf'                          => 'application/rtf',
@@ -55,7 +61,7 @@ function cul_allowed_upload_types($mimes) {
     	'ods'                          => 'application/vnd.oasis.opendocument.spreadsheet'
     );
 
-    update_option(CUL_ALLOWED_EXTENSIONS_KEY, json_encode($mimes));
+    update_option(CUL_ALLOWED_EXTENSIONS_KEY, json_encode($mimes, JSON_UNESCAPED_SLASHES));
   }
 
   $mimes = json_decode(get_option(CUL_ALLOWED_EXTENSIONS_KEY), true);
@@ -72,7 +78,7 @@ function cul_allowed_upload_file_extensions() {
 /** Set up settings page **/
 if ( is_admin() ){
   cul_allowed_upload_types(array()); // Call allowed types method to initialize default values if blank
-  
+
   add_action( 'admin_menu', 'cul_allowed_upload_types_plugin_menu' );
   add_action( 'admin_init', 'register_cul_allowed_upload_types_plugin_settings' );
 }
@@ -94,73 +100,121 @@ function cul_allowed_upload_types_plugin_options() {
   <script>
   var $ = jQuery;
   $(document).ready(function(){
-
     //Bind delete row action
     $('#cul-allowed-upload-types-form').on('click', '.delete-row', function(){
       $(this).closest('tr').remove();
     })
 
+    //Bind add row action
+    $('#cul-allowed-upload-types-form').on('click', '.add-row', function(){
+      $trToClone = $(this).closest('tr');
+      $clonedTr = $trToClone.clone();
+      $clonedTr.find('input').val('');
+      $trToClone.after($clonedTr);
+      $trToClone.find('button.add-row').removeClass('add-row').addClass('delete-row').html('x');
+    })
+
     //Bind submit action
     $('#cul-allowed-upload-types-form').on('submit', function(e){
-      var newUploadTypes = {};
 
-      $('#upload-type-values tbody').find('tr').each(function(){
-        var extension = $(this).find('td.extension input').val();
-        var mimeType = $(this).find('td.mime-type input').val();
+      if($(this).find('textarea#json-editor').length > 0) {
+        //Json editing mode
 
-        if(extension !== '' && mimeType !== '') {
-          newUploadTypes[extension] = mimeType;
+        //Validate json in json-editor textarea
+        try {
+          var newUploadTypes = JSON.parse($(this).find('textarea#json-editor').val());
+        } catch(err) {
+          alert('There is an error in your JSON. Please check and fix.');
+          return false;
         }
-      });
+      } else {
+        //Form editing mode
 
-      $('#hidden-data-field').val(JSON.stringify(newUploadTypes));
+        var newUploadTypes = {};
+
+        $('#upload-type-values tbody').find('tr').each(function(){
+          var extension = $(this).find('td.extension input').val();
+          var mimeType = $(this).find('td.mime-type input').val();
+
+          if(extension !== '' && mimeType !== '') {
+            newUploadTypes[extension] = mimeType;
+          }
+        });
+
+        $('#hidden-data-field').val(JSON.stringify(newUploadTypes));
+      }
 
       return true;
     });
   });
   </script>
-
   <div class="wrap">
     <h2>CUL Allowed Upload Types</h2>
   </div>
   <form id="cul-allowed-upload-types-form" method="post" action="options.php">
+
+    <div>
+      <?php if(isset($_GET['json']) && $_GET['json'] == 'true'): ?>
+        <a href="<?php echo strtok($_SERVER["REQUEST_URI"],'?') . '?page=' . esc_html($_GET['page']) . '&amp;json=false'; ?>">Edit as Form</a>
+      <?php else: ?>
+        <a href="<?php echo strtok($_SERVER["REQUEST_URI"],'?') . '?page=' . esc_html($_GET['page']) . '&amp;json=true'; ?>">Edit as JSON</a>
+      <?php endif; ?>
+    </div>
+
     <?php settings_fields( 'cul-allowed-upload-types-group' ); ?>
     <?php do_settings_sections( 'cul-allowed-upload-types-group' ); ?>
-    <table class="form-table">
-      <input id="hidden-data-field" type="hidden" name="<?php echo CUL_ALLOWED_EXTENSIONS_KEY; ?>" value="">
-      <tr>
-        <td>
-          <table id="upload-type-values">
-            <thead>
-              <tr>
-                <th>Extension</th>
-                <th>Mime Type</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-                if(get_option(CUL_ALLOWED_EXTENSIONS_KEY) == false) {
-                  $allowed_extensions = array();
-                } else {
-                  $allowed_extensions = json_decode(get_option(CUL_ALLOWED_EXTENSIONS_KEY), true);
-                }
-                $allowed_extensions[''] = ''; //add empty item at the end so we have a new, blank row at the end of the table
-                foreach($allowed_extensions as $extension => $mimeType):
-              ?>
-              <tr>
-                <td class="extension"><input type="text" value="<?php echo esc_html($extension); ?>"></td>
-                <td class="mime-type"><input type="text" value="<?php echo esc_html($mimeType); ?>"></td>
-                <?php if($extension != ''): ?>
-                  <td><button class="delete-row" type="button">x</button></td>
-                <?php endif; ?>
-              </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        </td>
-      </tr>
-    </table>
+
+    <?php
+      if(get_option(CUL_ALLOWED_EXTENSIONS_KEY) == false) {
+        $allowed_extensions = array();
+      } else {
+        $allowed_extensions = json_decode(get_option(CUL_ALLOWED_EXTENSIONS_KEY), true);
+        ksort($allowed_extensions);
+      }
+    ?>
+
+    <?php if(isset($_GET['json']) && $_GET['json'] == 'true'): ?>
+      <br />
+      <textarea id="json-editor" name="<?php echo CUL_ALLOWED_EXTENSIONS_KEY; ?>" rows="20" cols="100"><?php
+        echo json_encode($allowed_extensions, JSON_UNESCAPED_SLASHES);
+      ?></textarea>
+    <?php else: ?>
+      <div>
+        <p>Make sure to hit the save button after making changes to this list!</p>
+      </div>
+      <table class="form-table">
+        <input id="hidden-data-field" type="hidden" name="<?php echo CUL_ALLOWED_EXTENSIONS_KEY; ?>" value="">
+        <tr>
+          <td>
+            <table id="upload-type-values">
+              <thead>
+                <tr>
+                  <th>Extension</th>
+                  <th>Mime Type</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                  $allowed_extensions[''] = ''; //add empty item at the end so we have a new, blank row at the end of the table
+                  foreach($allowed_extensions as $extension => $mimeType):
+                ?>
+                <tr>
+                  <td class="extension"><input type="text" placeholder="extension" value="<?php echo esc_html($extension); ?>"></td>
+                  <td class="mime-type"><input type="text" placeholder="mime type" value="<?php echo esc_html($mimeType); ?>"></td>
+                  <?php if($extension != ''): ?>
+                    <td><button class="delete-row button button-secondary" type="button">x</button></td>
+                  <?php else: ?>
+                    <td><button class="add-row button button-secondary" type="button">Add</button></td>
+                  <?php endif; ?>
+                </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </table>
+    <?php endif; ?>
     <?php submit_button(); ?>
   </form>
   <?php
